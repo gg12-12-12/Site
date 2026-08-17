960ee045b256024b21d4f55b8930c395777b9a8e
gg12-12-12 <roma2101010@gmail.com>
2026-08-17 14:41:33 +0200

    Update CACHE_NAME to version v2

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * бачитимуть стару закешовану версію сайту!
  */
 
-const CACHE_NAME = 'holyliquid-v1';
+const CACHE_NAME = 'holyliquid-v2';
 
 // Файли, які кешуються одразу при встановленні.
 const PRECACHE_URLS = [

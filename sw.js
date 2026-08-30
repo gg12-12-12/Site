a5d11b1b3aba9cda0145cabeb147dafb3ddfa318
gg12-12-12 <roma2101010@gmail.com>
2026-08-30 14:30:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * бачитимуть стару закешовану версію сайту!
  */
 
-const CACHE_NAME = 'holyliquid-v33';
+const CACHE_NAME = 'holyliquid-v34';
 
 // Файли, які кешуються одразу при встановленні.
 const PRECACHE_URLS = [

fbb18acbefb5734b4443f3cc454e91a4eaae60be
gg12-12-12 <roma2101010@gmail.com>
2026-08-30 13:43:34 +0200

    Increment CACHE_NAME to 'holyliquid-v31'
    Updated CACHE_NAME to version v31 to refresh cache.

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * бачитимуть стару закешовану версію сайту!
  */
 
-const CACHE_NAME = 'holyliquid-v30';
+const CACHE_NAME = 'holyliquid-v31';
 
 // Файли, які кешуються одразу при встановленні.
 const PRECACHE_URLS = [

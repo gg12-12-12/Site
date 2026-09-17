f2279ff02adbb07d6bf81f336afaf81c1cb186cf
gg12-12-12 <roma2101010@gmail.com>
2026-09-17 23:39:45 +0200

    Change CACHE_NAME to holyliquid-v50
    Updated cache name to version 50 to refresh cached assets.

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * бачитимуть стару закешовану версію сайту!
  */
 
-const CACHE_NAME = 'holyliquid-v49';
+const CACHE_NAME = 'holyliquid-v50';
 
 // Файли, які кешуються одразу при встановленні.
 const PRECACHE_URLS = [
